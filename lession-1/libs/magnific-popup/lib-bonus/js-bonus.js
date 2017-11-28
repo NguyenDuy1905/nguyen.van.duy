5a745842bd643d1841fdf711147c92cf40689a53
NguyenDuy1905 <nguyenduy19051995@gmail.com>
2017-11-28 16:47:49 +0700

    Lession 1

---
+++ lession-1/libs/magnific-popup/lib-bonus/js-bonus.js
@@ -0,0 +1,44 @@
+
+
+// Magnific popup READY , Effect & Close popup
+
+$(document).ready(function() {
+    // Faded Zoom animation
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+
+        closeBtnInside: true,
+        preloader: false,
+        
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
+
+    // Faded Slide animation
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+
+        closeBtnInside: true,
+        preloader: false,
+        
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
+    // Close Popup
+    $('.close-popup').click(function() {
+        $.magnificPopup.close();
+    });
+});
